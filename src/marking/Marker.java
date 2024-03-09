3b8250dcaecf54b57a370d1f7a73a1366b283bb8
Bryan's Project 101s <bryanchiamjf01@gmail.com>
2024-03-09 19:10:46 +1030

    started section 2, created few methods in fields
    methods such as get, getValue and toString have been created but the output is not accurate just yet

--- src/marking/Marker.java
+++ src/marking/Marker.java
@@ -35,7 +35,7 @@ public class Marker {
 		// Section TWO
 		// uncomment this section once you get to it
 		
-		/*
+
 		Field testField = new Field(5, 5);
 		System.out.println(testField.get(3, 3)); //should print "."
 		System.out.println(testField); //should print the field
@@ -48,7 +48,7 @@ public class Marker {
 		testField.till(0, 0);
 		System.out.println(a.equals(testField.get(0, 0))); //should print false
 		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
-		*/
+
 
 		// Section THREE
 		// uncomment this section once you get to it
--- src/students/Field.java
+++ src/students/Field.java
@@ -1,10 +1,56 @@
 package students;
 
+import students.items.*;
 
 public class Field {
 	
-	public Field(int height, int width)
-	{
+	private int height;
+	private int width;
+	private Item[][] field;
+	
+	public Field(int height, int width) {
+		
+		this.height = height;
+		this.width = width;
+		this.field = new Item[height][width];
+		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				field[i][j] = new Soil();
+			}
+			
+		}
+		
+	}
+	
+	public String get(int x, int y) {
+		return field[x][y].toString();
+	}
+	
+	public int getValue() {
+		
+		public int totalValue;
+		
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				totalValue += field[i][j].getValue();
+			}
+		}
+		return totalValue;
+	}
+	
+	public String toString() {
+		StringBuilder fieldDisplay = new StringBuilder();
+		for (int i = 0; i < width; i++) {
+			fieldDisplay.append(i);
+		}
+		for (int i = 0; i < height; i++) {
+			fieldDisplay.append(i + 1);
+			for (int j = 0; j < width; j++) {
+				fieldDisplay.append(field[i][j].toString());
+			}
+		return fieldDisplay.toString();
+		}
 	}
 	
 }
